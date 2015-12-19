1bd355a2d07f8892bd29a2df4e6cb8802c8d4d3a
blf2 <blf20822@126.com>
2015-12-19 20:03:22 +0800

    create enum of UserSex

--- src/net/model/staticFile/enumFile/UserSex.java
+++ src/net/model/staticFile/enumFile/UserSex.java
@@ -4,4 +4,17 @@ package net.model.staticFile.enumFile;
  * Created by blf2 on 15-12-19.
  */
 public enum UserSex {
+    men,women;
+
+    public boolean isMen(){
+        if(this == men)
+            return true;
+        return false;
+    }
+
+    public  boolean isWomen(){
+        if(this == women)
+            return true;
+        return false;
+    }
 }
