7edbd3da48e858fd4059bb5dd40bf960fa490acc
blf2 <blf20822@126.com>
2015-12-19 19:59:18 +0800

    create DAO

---
+++ src/net/model/DAO/DbInsert.java
@@ -0,0 +1,7 @@
+package net.model.DAO;
+
+/**
+ * Created by root on 15-12-19.
+ */
+public class DbInsert {
+}
---
+++ src/net/model/staticFile/enumFile/UserSex.java
@@ -0,0 +1,7 @@
+package net.model.staticFile.enumFile;
+
+/**
+ * Created by blf2 on 15-12-19.
+ */
+public enum UserSex {
+}
