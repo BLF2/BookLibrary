aced4bd88a0e1b213d965bc48384e31ab22583da
blf2 <blf20822@126.com>
2015-12-19 20:01:26 +0800

    create interface

---
+++ src/net/model/staticFile/interfaceFile/IUser.java
@@ -0,0 +1,20 @@
+package net.model.staticFile.interfaceFile;
+
+import net.model.staticFile.enumFile.UserRule;
+import net.model.staticFile.enumFile.UserSex;
+
+/**
+ * Created by blf2 on 15-12-19.
+ */
+public interface IUser {
+    Integer getUserId();
+    String getUserCardNum();
+    String getUserName();
+    UserSex getUserSex();
+    String getUserStuNum();
+    UserRule getUserRule();
+    boolean isAdmin();
+    boolean isUser();
+    boolean isMen();
+    boolean isWomen();
+}
