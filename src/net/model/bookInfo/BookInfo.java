ceb0197dbcb372d8a6f855b0c9795ea4274c3a93
blf2 <blf20822@126.com>
2015-12-19 20:00:30 +0800

    create bookInfo

---
+++ src/net/model/bookInfo/BookInfo.java
@@ -0,0 +1,9 @@
+package net.model.bookInfo;
+
+/**
+ * Created by blf2 on 15-12-19.
+ */
+public class BookInfo {
+    
+
+}
---
+++ src/net/model/bookInfo/BookInfoFactory.java
@@ -0,0 +1,7 @@
+package net.model.bookInfo;
+
+/**
+ * Created by root on 15-12-19.
+ */
+public class BookInfoFactory {
+}
